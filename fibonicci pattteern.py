d92315e6d57c5f9f5609fe072caf195cf000e016
Syed-Amash-07 <syedamash.tce@gmail.com>
2024-01-09 00:49:11 +0530

    added new repository

---
+++ fibonicci pattteern.py
@@ -0,0 +1,12 @@
+ab=2
+for i in range(5):
+    a=1
+    b=1
+    print()
+    for j in range(ab):
+        print(a,end='')
+        ba=a+b
+        a=b
+        b=ba
+    ab==1        
+        
---
+++ return.py
@@ -0,0 +1,6 @@
+def multiply(number1,number2):
+    result=number1*number2
+    return result
+
+x=multiply(6,8)
+print(x)
