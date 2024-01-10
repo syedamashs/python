1b230d6c1572bb8aa06bdd8adda996de04566f4b
Syed-Amash-07 <syedamash.tce@gmail.com>
2024-01-10 22:12:44 +0530

    str format

---
+++ str_format.py
@@ -0,0 +1,14 @@
+c='hi'
+a='bro'
+b='good bye'
+z=3.1453656
+x=1000
+print("{} hello {} how are you {:<10} ok {}".format(c,a,a,b))
+print("{} hello {} how are you {:>10} ok {}".format(c,a,a,b))
+print("{} hello {} how are you {:^10} ok {}".format(c,a,a,b))
+print("the value of pi is {:.2f}".format(z))
+print("the number is {:,}".format(x))
+print("the number is {:b}".format(x))
+print("the number is {:o}".format(x))
+print("the number is {:X}".format(x))
+print("the number is {:E}".format(x))  
